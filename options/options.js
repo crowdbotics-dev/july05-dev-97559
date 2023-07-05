7b839584401de94e42c183b315387008691db5e0
Ravinder QA Lead <ravinder.y@crowdbotics.com>
2023-07-05 13:32:28 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "july05-dev-97559",
   url: "https://july05-dev-97559.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
--- store/julydevAPI/api.js
+++ store/julydevAPI/api.js
@@ -1,68 +1,96 @@
-import axios from "axios"
+import axios from "axios";
 const julydevAPI = axios.create({
   baseURL: "https://july05-dev-97559.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
-  return julydevAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
+  return julydevAPI.get(`/api-docs/schema/`, {
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return julydevAPI.post(`/api/v1/login/`, payload.data)
+  return julydevAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_mithilrs_list(payload) {
-  return julydevAPI.get(`/api/v1/mithilrs/`)
+  return julydevAPI.get(`/api/v1/mithilrs/`);
 }
+
 function api_v1_mithilrs_create(payload) {
-  return julydevAPI.post(`/api/v1/mithilrs/`, payload.data)
+  return julydevAPI.post(`/api/v1/mithilrs/`, payload.data);
 }
+
 function api_v1_mithilrs_retrieve(payload) {
-  return julydevAPI.get(`/api/v1/mithilrs/${payload.id}/`)
+  return julydevAPI.get(`/api/v1/mithilrs/${payload.id}/`);
 }
+
 function api_v1_mithilrs_update(payload) {
-  return julydevAPI.put(`/api/v1/mithilrs/${payload.id}/`, payload.data)
+  return julydevAPI.put(`/api/v1/mithilrs/${payload.id}/`, payload.data);
 }
+
 function api_v1_mithilrs_partial_update(payload) {
-  return julydevAPI.patch(`/api/v1/mithilrs/${payload.id}/`, payload.data)
+  return julydevAPI.patch(`/api/v1/mithilrs/${payload.id}/`, payload.data);
 }
+
 function api_v1_mithilrs_destroy(payload) {
-  return julydevAPI.delete(`/api/v1/mithilrs/${payload.id}/`)
+  return julydevAPI.delete(`/api/v1/mithilrs/${payload.id}/`);
 }
+
 function api_v1_signup_create(payload) {
-  return julydevAPI.post(`/api/v1/signup/`, payload.data)
+  return julydevAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function rest_auth_login_create(payload) {
-  return julydevAPI.post(`/rest-auth/login/`, payload.data)
+  return julydevAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return julydevAPI.get(`/rest-auth/logout/`)
+  return julydevAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return julydevAPI.post(`/rest-auth/logout/`)
+  return julydevAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return julydevAPI.post(`/rest-auth/password/change/`, payload.data)
+  return julydevAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return julydevAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return julydevAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return julydevAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+  return julydevAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_create(payload) {
-  return julydevAPI.post(`/rest-auth/registration/`, payload.data)
+  return julydevAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return julydevAPI.post(`/rest-auth/registration/verify-email/`, payload.data)
+  return julydevAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return julydevAPI.get(`/rest-auth/user/`)
+  return julydevAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return julydevAPI.put(`/rest-auth/user/`, payload.data)
+  return julydevAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return julydevAPI.patch(`/rest-auth/user/`, payload.data)
+  return julydevAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -84,4 +112,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
